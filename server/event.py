8df20b542b7ab7626c74dba139d64532ac680d7b
Drew Harry <drew.harry@gmail.com>
2010-06-14 18:38:25 -0400

    A bunch of heft added to the event class to get it ready for making new events. It's not tested yet, but wanted to isolate this from the upcoming changes to state.

--- server/event.py
+++ server/event.py
@@ -12,15 +12,24 @@ Copyright (c) 2010 MIT Media Lab. All rights reserved.
 
 import logging
 import state
-
+import time
+import uuid
 import simplejson as json
 
-EVENT_TYPES = ["NEW_MEETING", "JOINED", "LEFT", "NEW_TASK", "UPDATE_TASK", "NEW_TOPIC", "UPDATE_TOPIC", "PING"]
+
+EVENT_TYPES = ["JOINED", "LEFT", "NEW_TASK", "UPDATE_TASK", "NEW_TOPIC", "UPDATE_TOPIC", "PING"]
+
+# Stores the required paramters for each event type. We'll use this
+# to enforce complete intialization of events, and also to remind ourselves
+# what's required for each event.
+EVENT_PARAMS = {"JOINED":["user"],
+                "LEFT":["user"]
+                }
 
 class Event:
     
     
-    def __init__(self, eventType, meetingUUID, userUUID):
+    def __init__(self, eventType, meetingUUID, userUUID, params):
         
         # these are the only required fields for an event.
         # all other paramters (like the text of a new topic, or new owner
@@ -33,6 +42,14 @@ class Event:
         
         self.eventType = eventType
         
+        # log the time the event was created on the server.
+        self.timestamp = time.time()
+        
+        # give the event a uuid. Not 100% sure what we're going to use this
+        # for yet, but I've got a feeling we'll want it for ACKs from the
+        # client. 
+        self.uuid = str(uuid.uuid4())
+        
         # Eventually we'll be rigorous about checking these, but for now
         # if we get key errors, just eat them and set them to None. Too hard
         # to test without this for now.
@@ -42,6 +59,27 @@ class Event:
         except:
             self.meeting = None
             self.users = None
+            
+        # now, cycle through the params. These are the bonus event options
+        # that aren't shared by all events. The list of expected params is in
+        # event.EVENT_PARAMS. We're going to store them as a dict locally,
+        # but first we'll validate that we have at least the minimum expected
+        # for our event type.
+        expectedParams = EVENT_PARAMS[self.eventType]
+        hasAllRequiredParams = True
+        for param in expectedParams:
+            if(param not in params.keys()):
+                hasAllRequiredParams = False
+                missingParam = param
+        
+        # if we're missing a param, fail with a descriptive error message.
+        # otherwise, store the params and finish the constructor happily.
+        if(not hasAllParams):
+            logging.error("Tried to create event of type %s without param %s. Expects: %s"%(self.eventType, missingParam, expectedParams))
+            return None
+        else:
+            self.params = params
+        
         
         
     # TODO need to do some fancy stuff here to override the blah.foo operator so
@@ -50,8 +88,19 @@ class Event:
     
     def getJSON(self):
         d = {}
-        d["eventType"] = self.eventType
         
+        # I think I could some fancy self-referential loop-through-own-keys
+        # thing here, but I kinda like the explicitness of doing it by hand
+        # to remind what will actually be in this dictionary.
+        d["eventType"] = self.eventType
+        d["timestamp"] = self.timestamp
+        d["uuid"] = self.uuid
+        
+        # load in the params. Should they be in a separate namespace? 
+        # Going to go with same namespace for now. 
+        for key in self.params.keys():
+            d[key] = self.params[key]
+            
         # as above, this is just a temporary work around. Later, we'll enforce
         # these objects' existence
         try:
