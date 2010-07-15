1379a2a9f86ff6c59baec0c041e8285a067bab0e
Drew Harry <drew.harry@gmail.com>
2010-07-15 14:29:20 -0400

    A little more progress on ipad synchronization.

---
+++ ipad/Classes/Location.h
@@ -0,0 +1,16 @@
+//
+//  Location.h
+//  TinCan
+//
+//  Created by Drew Harry on 7/15/10.
+//  Copyright 2010 MIT Media Lab. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "SynchronizedObject.h"
+
+@interface Location : SynchronizedObject {
+
+}
+
+@end
---
+++ ipad/Classes/Location.m
@@ -0,0 +1,14 @@
+//
+//  Location.m
+//  TinCan
+//
+//  Created by Drew Harry on 7/15/10.
+//  Copyright 2010 MIT Media Lab. All rights reserved.
+//
+
+#import "Location.h"
+
+
+@implementation Location
+
+@end
