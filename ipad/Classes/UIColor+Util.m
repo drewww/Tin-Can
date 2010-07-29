d01816f298894cc9ae411952fa606f0beb4faf49
Paula Jacobs <paulaj@mit.edu>
2010-07-29 16:39:51 -0400

    Fixed a bug where meetings weren't being unswizzled. Also, added in some code from the internet to do color conversions from hex to UIColor.

--- ipad/Classes/UIColor+Util.m
+++ ipad/Classes/UIColor+Util.m
@@ -53,7 +53,29 @@
     
 	return retColor;
 }
--(UIColor *)convertToUIColorWithHex:(NSString *)hexidecimal{
+//method found at: http://github.com/jonasschnelli/UIColor-i7HexColor
++ (UIColor *)colorWithHexString:(NSString *)hexString {
+	
+	/* convert the string into a int */
+	unsigned int colorValue;
+	NSString *hexStringCleared = [hexString stringByReplacingOccurrencesOfString:@"#" withString:@""];
+	if(hexStringCleared.length == 3) {
+		/* short color form */
+		/* im lazy, maybe you have a better idea to convert from #fff to #ffffff */
+		hexStringCleared = [NSString stringWithFormat:@"%@%@%@%@%@%@", [hexStringCleared substringWithRange:NSMakeRange(0, 1)],[hexStringCleared substringWithRange:NSMakeRange(0, 1)],
+							[hexStringCleared substringWithRange:NSMakeRange(1, 1)],[hexStringCleared substringWithRange:NSMakeRange(1, 1)],
+							[hexStringCleared substringWithRange:NSMakeRange(2, 1)],[hexStringCleared substringWithRange:NSMakeRange(2, 1)]];
+	}
+	if(hexStringCleared.length == 6) {
+		hexStringCleared = [hexStringCleared stringByAppendingString:@"ff"];
+	}
+	[[NSScanner scannerWithString:hexStringCleared] scanHexInt:&colorValue];
+	
+	/* now build the UIColor with standard allocators */
+	return [UIColor colorWithRed:((colorValue)&0xFF)/255.0 
+						   green:((colorValue>>8)&0xFF)/255.0 
+							blue:((colorValue>>16)&0xFF)/255.0 
+						   alpha:((colorValue>>24)&0xFF)/255.0];
 	
 }
 
