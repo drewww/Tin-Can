9fe070a26e1f989e1ef374c1e74d76e4d689cf2c
Paula Jacobs <paulaj@mit.edu>
2010-08-10 16:26:49 -0400

    made header proportional to height in containers

--- ipad/Classes/TaskContainerView.m
+++ ipad/Classes/TaskContainerView.m
@@ -38,10 +38,10 @@
 	
 	
 	CGContextSetFillColorWithColor(ctx, [UIColor colorWithRed:.5 green:.5 blue:.5 alpha:1].CGColor);
-	CGContextFillRect(ctx, CGRectMake(0, 0, self.frame.size.width, self.bounds.size.height/25.0));
+	CGContextFillRect(ctx, CGRectMake(0, 0, self.bounds.size.width, self.bounds.size.height/22.0));
 	CGContextSetFillColorWithColor(ctx, [UIColor whiteColor].CGColor);
-	[@"TASKS" drawInRect:CGRectMake(0, self.bounds.size.height/150.0, self.frame.size.width, self.bounds.size.height/25.0 - self.bounds.size.height/100.0) 
-                withFont:[UIFont boldSystemFontOfSize:18] lineBreakMode:UILineBreakModeTailTruncation alignment:UITextAlignmentCenter];
+	[@"TASKS" drawInRect:CGRectMake(0, self.bounds.size.height/150.0, self.bounds.size.width, self.bounds.size.height/25.0 - self.bounds.size.height/100.0) 
+                withFont:[UIFont boldSystemFontOfSize:self.bounds.size.height/33.3] lineBreakMode:UILineBreakModeTailTruncation alignment:UITextAlignmentCenter];
 	
 	CGContextSetLineWidth(ctx,2);
 	CGContextSetStrokeColorWithColor(ctx,  [UIColor colorWithRed:.5 green:.5 blue:.5 alpha:1].CGColor);
