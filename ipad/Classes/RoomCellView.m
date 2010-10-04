3bbb31304891f7d5647f2e23023b934c1fe2ecc2
Drew Harry <drew.harry@gmail.com>
2010-10-04 16:11:03 -0400

    More progress. TimerBars are back in, plus placement is closer to final.

--- ipad/Classes/RoomCellView.m
+++ ipad/Classes/RoomCellView.m
@@ -28,8 +28,26 @@
 //Setter for Room
 - (void) setRoom:(Room *)newRoom {
     room = newRoom;
-    
-    // Move the meeting code into here, since it's all the same.
+
+    // Set up the timer bar.
+    if(room.currentMeeting != nil) {
+        
+        
+        // For now, this is all bad data. We'll make it work off real data soon. 
+    	NSDate *startingTime = [NSDate date];
+    	NSLog(@"starting time in seconds: %f", [startingTime timeIntervalSince1970]);
+    	NSTimeInterval startingTimeInSeconds = [startingTime timeIntervalSince1970] -1800;
+    	
+    	NSMutableArray *times=[[NSMutableArray alloc] initWithCapacity:6];
+    	[times addObject:[NSDate dateWithTimeIntervalSince1970:[[NSDate date] timeIntervalSince1970]-1000]];
+    	[times addObject:[NSDate dateWithTimeIntervalSince1970:[[NSDate date] timeIntervalSince1970]-600]];
+    	[times addObject:[NSDate dateWithTimeIntervalSince1970:[[NSDate date] timeIntervalSince1970]-400]];
+    	
+    	TimerBar *timerBar=[[TimerBar alloc]initWithFrame: CGRectMake(10, 80, 270, 10) withStartTime:[NSDate dateWithTimeIntervalSince1970:startingTimeInSeconds] 
+    							 withEventTimes:times];
+    	
+    		[self addSubview:timerBar];
+    	}   
     
 }
 
@@ -79,24 +97,32 @@
         
         // Grab the meeting name.
         meetingTitle = room.currentMeeting.title;
+        
+        if([meetingTitle isKindOfClass:[NSNull class]]) {
+                meetingTitle = @"Untitled Meeting";
+        }
+        
         numParticipants = [room.currentMeeting.currentParticipants count];
         
     } else {
         NSLog(@"No meeting in this room, just put up a 'start a meeting' notice.");
         
-        meetingTitle = @"Start a new meeting";
+        meetingTitle = @"Start a new meeting with a really long wrapping test name";
         numParticipants = 0;
     }
     
     CGContextSetFillColorWithColor(ctx, [UIColor blackColor].CGColor);
-    [meetingTitle drawInRect:CGRectMake(10, 10, 210, 20) withFont:[UIFont systemFontOfSize:24]];
+    [meetingTitle drawInRect:CGRectMake(10, 10, 270, 46) withFont:[UIFont systemFontOfSize:20]];
+    
+//    CGContextSetStrokeColorWithColor(ctx, [UIColor blackColor].CGColor);
+//    CGContextStrokeRect(ctx, CGRectMake(10, 10, 270, 60));
     
     // Now do the generic stuff, like drawing the room name and participant counter.
     
     CGRect roomNameRect = CGRectMake(290, 10, 80, 20);
-    CGRect peopleCountRect = CGRectMake(290, 30, 80, 30);
-    CGRect peopleLabelRect = CGRectMake(290, 60, 80, 20);
-    CGRect totalRect = CGRectMake(290, 10, 80, 70);
+    CGRect peopleCountRect = CGRectMake(290, 30, 80, 40);
+    CGRect peopleLabelRect = CGRectMake(290, 70, 80, 20);
+    CGRect totalRect = CGRectMake(290, 10, 80, 80);
     
     CGContextSetFillColorWithColor(ctx, [UIColor blueColor].CGColor);
     CGContextFillRect(ctx, roomNameRect);
@@ -110,11 +136,9 @@
     [[room.name uppercaseString] drawInRect:roomNameRect withFont:[UIFont boldSystemFontOfSize:16] lineBreakMode:UILineBreakModeWordWrap alignment:UITextAlignmentCenter];
     [@"people" drawInRect:peopleLabelRect withFont:[UIFont systemFontOfSize:16] lineBreakMode:UILineBreakModeWordWrap alignment:UITextAlignmentCenter];
     
-
-    
     CGContextSetFillColorWithColor(ctx, [UIColor blackColor].CGColor);
     NSString *participantsCountString = [NSString stringWithFormat:@"%d", numParticipants];
-    [participantsCountString drawInRect:peopleCountRect withFont:[UIFont systemFontOfSize:20] lineBreakMode:UILineBreakModeWordWrap alignment:UITextAlignmentCenter];  
+    [participantsCountString drawInRect:peopleCountRect withFont:[UIFont systemFontOfSize:30] lineBreakMode:UILineBreakModeWordWrap alignment:UITextAlignmentCenter];  
      
     
     
