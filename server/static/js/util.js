b07de8ba2033454761110a558c36c5b85781efa3
Joshua Ma <me@joshma.com>
2010-10-25 17:48:02 -0400

    ported timesince in util.py to util.js, doesn't show seconds (e.g. 5h 3m ago) and abbreviated format. adding new task in demo.html now adds a new li element with the appropriate created by, how long ago, and 'Not assigned' (assuming new task isn't assigned)

--- server/static/js/util.js
+++ server/static/js/util.js
@@ -12,3 +12,29 @@ function array_remove(array, obj) {
         return array;
     }
 }
+
+function timesince(d, now) {
+    //Takes two timestamps (in seconds) && returns time between d && now
+    //as a formatted string, e.g. "10h ago", but only goes up to hours
+    //so an output like "74h and 3m ago" is possible.
+    now = (typeof now == 'undefined') ? new Date().getTime()/1000 : now;
+    
+    var diff = Math.round(now - d);
+    var hours = Math.floor(diff/3600);
+    var minutes = Math.floor((diff-hours*3600)/60);
+    var seconds = Math.floor(diff-hours*3600-minutes*60);
+    
+    var o = "";
+    if (hours>1) o = hours+"h";
+    else if (hours==1) o = "1h";
+        
+    if (o!="" && minutes>0) o += " and ";
+        
+    if (minutes>1) o += minutes+"m";
+    else if (minutes==1) o += "1m";
+    
+    if (o!="") o += " ago";
+    else o = "just now";
+    
+    return o;
+}
--- server/util.py
+++ server/util.py
@@ -13,45 +13,35 @@ Copyright (c) 2010 MIT Media Lab. All rights reserved.
 import time
 
 def timesince(d, now=None):
-	"""
-	Takes two time objects and returns time between d and now
-	as a formatted string, e.g. "10 minutes ago", but only goes up to hours
-	so an output like "74 hours, 3 minutes, 2 seconds ago" is possible.
-	"""
-	if not now:
-		now = time.time()
-	
-	diff = int(now - d);
-	hours = diff/3600
-	minutes = (diff-hours*3600)/60
-	seconds = diff-hours*3600-minutes*60
-	
-	o = ""
-	if (hours>1):
-	    o = str(hours)+" hours"
-	elif (hours==1):
-	    o = "1 hour"
-	
-	if (o!="" and minutes>0 and seconds>0):
-	    o += ", "
-	elif (o!="" and (minutes>0 or seconds>0)):
-	    o += " and "
-	
-	if (minutes>1):
-	    o += str(minutes)+" minutes"
-	elif (minutes==1):
-	    o += "1 minute"
-	
-	if (hours>0 and seconds>0):
-	    o += ", and "
-	elif (seconds>0):
-	    o += " and "
-	
-	if (seconds>1):
-	    o += str(seconds)+" seconds"
-	elif (seconds==1):
-	    o += "1 second"
-	
-	o += " ago"
-	
-	return o
+    """
+    Takes two time objects and returns time between d and now
+    as a formatted string, e.g. "10m ago", but only goes up to hours
+    so an output like "74h and 3m ago" is possible.
+    """
+    if not now:
+        now = time.time()
+    
+    diff = int(now - d);
+    hours = diff/3600
+    minutes = (diff-hours*3600)/60
+    
+    o = ""
+    if (hours>1):
+        o = str(hours)+"h"
+    elif (hours==1):
+        o = "1h"
+        
+    if (o!="" and minutes>0):
+        o += " and "
+        
+    if (minutes>1):
+        o += str(minutes)+"m"
+    elif (minutes==1):
+        o += "1m"
+
+    if (o!=""):
+        o += " ago"
+    else:
+        o = "just now"
+    
+    return o
