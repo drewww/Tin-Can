002b6894d1ac869429ea76163a5dd50ca14f0b9f
Drew Harry <drew.harry@gmail.com>
2011-03-09 14:58:02 -0500

    Basic sugar for interacting with SMTP server more easily.

--- server/mail.py
+++ server/mail.py
@@ -13,23 +13,26 @@ Copyright (c) 2011 MIT Media Lab. All rights reserved.
 
 import smtplib
 import logging
+import util
 
 from email.mime.text import MIMEText
 
-smtp_server = smtplib.SMTP()
+smtp_server = None
 
-if __name__ == '__main__':
-    logging.debug("about to try sending an email")
+def sendmail(to, subject, body):
+    global smtp_server
+    
+    if smtp_server==None:
+        # set up a new one
+        smtp_server = smtplib.SMTP(util.config.get("email", "smtp_server"))
 
-    # try sending a trivial 
-    me = "foo@mit.edu"
-    you = "drew.harry@gmail.com"
+    from_email = util.config.get("email", 'from_email')
+
+    msg = MIMEText(body)
     
-    msg = MIMEText("This is the body of the email.")
+    msg['Subject'] = subject
+    msg['From'] = from_email
+    msg['To'] = to
     
-    msg['Subject'] = "EMAIL. IT WORKS."
-    msg['From'] = me
-    msg['To'] = you
-    
-    smtp_server.sendmail(me, [you], msg.as_string())
+    smtp_server.sendmail(from_email, [to], msg.as_string())
     
