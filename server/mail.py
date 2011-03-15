ea9a109c62ae109c24442dc2dfd6d9e4e102cf32
Drew Harry <drew.harry@gmail.com>
2011-03-15 13:04:41 -0400

    Added some try-catch on email sending to avoid being offline crashing the whole process.

--- server/mail.py
+++ server/mail.py
@@ -22,17 +22,20 @@ smtp_server = None
 def sendmail(to, subject, body):
     global smtp_server
     
-    if smtp_server==None:
-        # set up a new one
-        smtp_server = smtplib.SMTP(util.config.get("email", "smtp_server"))
+    try:
+        if smtp_server==None:
+            # set up a new one
+            smtp_server = smtplib.SMTP(util.config.get("email", "smtp_server"))
 
-    from_email = util.config.get("email", 'from_email')
+        from_email = util.config.get("email", 'from_email')
 
-    msg = MIMEText(body)
+        msg = MIMEText(body)
     
-    msg['Subject'] = subject
-    msg['From'] = from_email
-    msg['To'] = to
+        msg['Subject'] = subject
+        msg['From'] = from_email
+        msg['To'] = to
     
-    smtp_server.sendmail(from_email, [to], msg.as_string())
+        smtp_server.sendmail(from_email, [to], msg.as_string())
+    except Exception, e:
+        logging.warning("Error sending email: " + str(e))
     
