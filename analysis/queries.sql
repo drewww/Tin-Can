c15c55467506d1d39269f9915f6677347da70c9b
Drew Harry <drew.harry@gmail.com>
2011-05-03 18:14:47 -0400

    Added some first pass queries for exploring the data.

---
+++ analysis/queries.sql
@@ -0,0 +1,14 @@
+-- A handy place to save useful queries for analysis work.
+
+-- shows number of events per user. (events is a bit broad, though)
+select name, count(*) from events
+    join actors on actors.id = events.actor_id
+    group by actors.id;
+
+-- shows the distribution of events for all users
+SELECT type, count(*) from events group by type;
+
+-- distribution of events for all users.
+SELECT name, type, count(*) from events
+    join actors on actors.id = events.actor_id
+    group by actors.id, type
