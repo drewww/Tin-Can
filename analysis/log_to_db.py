a09b8b466460a8d7ed499cf7f7ed2e684238f721
Drew Harry <drew.harry@gmail.com>
2011-05-03 15:24:55 -0400

    Connects to DB properly.

--- analysis/log_to_db.py
+++ analysis/log_to_db.py
@@ -12,10 +12,27 @@ Created by Drew Harry on 2011-05-03.
 Copyright (c) 2011 MIT Media Lab. All rights reserved.
 """
 
-import sys
-import os
+import MySQLdb
+
+# this will keep a reference to the database connection.
+db = None
+
+
+def convert_log(path):
+    f = open(path, 'r')
+    
+    
+    
 
 
 if __name__ == '__main__':
-    print "running!"
+    print "Opening database connection."
+    db = MySQLdb.connect(host = "localhost", user="root", db="tincan")
+    
+    
+    convert_log("emerson-logs/first_class.log")
+    
+    print "Closing database connection."
+    db.close()
+    
 
