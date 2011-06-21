1d01df56a027cbe64cb563338e9524ad23d29d96
Drew Harry <drew.harry@gmail.com>
2011-06-21 16:22:29 -0400

    Added a method on the connection manager to do thumbs up.

--- server/static/js/ConnectionManager.js
+++ server/static/js/ConnectionManager.js
@@ -915,6 +915,26 @@ ConnectionManager.prototype = {
         });
     },
     
+    thumbsUp: function(){
+        
+        // We don't need to include any data because our device cookie
+        // will identify us to the server.
+        $.ajax({
+            url: '/users/thumb',
+            type: "POST",
+            context: this,
+            data: {},
+            success: function () {
+                this.publishEvent(this.generateEvent("THUMBS_UP_COMPLETE",
+                    {}));
+            },
+            error: function() {
+                this.publishEvent(this.generateEvent("THUMBS_UP_COMPLETE",
+                    {}, false));
+            }
+        });
+    },
+    
     // Returns the meeting that this client is currently in. Might be null,
     // if this client hasn't joined a meeting yet. 
     getCurrentMeeting: function() {
